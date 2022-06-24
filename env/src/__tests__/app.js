d4ac56c325f5058d53d9dc1fdfe95d5cd95bed05
Redhead80 <g.utkin80@yandex.ru>
2022-06-24 23:50:58 +0300

    commit

--- env/src/__tests__/app.js
+++ env/src/__tests__/app.js
@@ -1,6 +1,6 @@
-import demo from "../js/app";
+import demo from '../js/app';
 
-test('demo is demo', ()=>{
-    let result = demo(2);
-    expect(result).toBe(2);
-})
+test('demo is demo', () => {
+  const result = demo(2);
+  expect(result).toBe(2);
+});
